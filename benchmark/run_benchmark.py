7733055607da80919b27e276fc5bb021e1247a54
Andreas Lundell <andreas.lundell@abo.fi>
2025-10-22 15:40:10 +0000

    update run_benchmark.py

--- benchmark/run_benchmark.py
+++ benchmark/run_benchmark.py
@@ -3,6 +3,7 @@ import os
 import csv
 import subprocess
 import argparse
+import xml.etree.ElementTree as ET
 
 
 def safe_name(name: str) -> str:
@@ -45,40 +46,102 @@ def main():
     with open(problem_list, newline='') as csvfile:
         filtered_lines = (line for line in csvfile if not line.strip().startswith("#"))
         reader = csv.DictReader(filtered_lines)
-
+        
         for row in reader:
-            problem_file = row["problem_filename"]
-            order = row["order"]
-            assign = row["assign"]
-
-            # Build subdirectory name
-            subfolder_name = f"{safe_name(problem_file)}-{order}_{assign}"
-            problem_output_dir = os.path.join(output_dir, subfolder_name)
-            
-            # Skip if already exists
-            if os.path.exists(problem_output_dir):
-                print(f"Skipping {problem_file} (already exists: {problem_output_dir})")
-                continue
-            
-            os.makedirs(problem_output_dir, exist_ok=True)
+            problem_file = "problems/"+row["problem_filename"]+ ".gms"
+            # order = row["order"]
+            # quiet = row["quiet"]
+            # sparsity = row["sparsity"]
+            # useTSSOS = row["useTSSOS"]
+            # tolSOS = row["tolSOS"]
             quiet = "true"
-            log_file = os.path.join(problem_output_dir, "SHOT.log")
-            # Build command
-            cmd = [
-                executable,
-                problem_file,
-                problem_output_dir,
-                "SHOT.opt",
-                order,
-                quiet,
-                assign
-            ]
+            useTSSOS = "false"
+            tolSOS = "1e-6"
+            for order in ["2","3"]:
+                for sparsity in ["all","cliques","single"]:
 
-            print(f"Running: {' '.join(cmd)}")
-            try:
-                subprocess.run(cmd, check=True)
-            except subprocess.CalledProcessError as e:
-                print(f"Error while running {cmd}: {e}")
+                    # Build subdirectory name
+                    subfolder_name = f"{safe_name(problem_file)}-{order}_{sparsity}"
+                    problem_output_dir = os.path.join(output_dir, subfolder_name)
+
+                    # Skip if already exists
+                    if os.path.exists(problem_output_dir):
+                        print(f"Skipping {problem_file} (already exists: {problem_output_dir})")
+                        continue
+                    
+                    os.makedirs(problem_output_dir, exist_ok=True)
+                    quiet = "true"
+                    log_file = os.path.join(problem_output_dir, "SHOT.log")
+                    # Build command
+                    cmd = [
+                        executable,
+                        problem_file,
+                        problem_output_dir,
+                        "SHOT.opt",
+                        order,
+                        quiet,
+                        sparsity,
+                        useTSSOS,
+                        tolSOS
+                    ]
+
+                    print(f"Running: {' '.join(cmd)}")
+                    try:
+                        subprocess.run(cmd, check=True)
+                    except subprocess.CalledProcessError as e:
+                        print(f"Error while running {cmd}: {e}")
+    
+    # output_values_dict = {'time':['Total', 'Julia initialize', 'Lasserre total','CallbackExternalHyperplaneGeneration'], 'other':['DualObjectiveBound'] }
+    # print_sol(output_dir,output_values_dict)
+
+def print_sol(output_dir, output_values_dict):
+    # Ensure output_dir exists
+    os.makedirs(output_dir, exist_ok=True)
+    
+    # get list of all subdirectories
+    folders = [f for f in os.listdir(output_dir) if os.path.isdir(os.path.join(output_dir, f))]
+    folders.sort()
+
+    # store output values
+    list_header = ["problem"]
+    list_header.extend([v for k in  output_values_dict.keys() for v in output_values_dict[k]]) 
+    sol_list = [list_header]
+
+    for f in folders: 
+        sol_prob = [f]
+        output_dir_problem = os.path.join(output_dir,f)
+        
+        #check if problem was solved
+        try:
+            tree = ET.parse(os.path.join(output_dir_problem,"SHOT.osrl"))
+        except FileNotFoundError:
+            print(f"No solution file found for problem {f}.")
+            continue
+        
+        root = tree.getroot()
+
+        # define namespace 
+        ns = {"os": "os.optimizationservices.org"}
+        
+        # get values form SHOT.osrl
+        for k in output_values_dict.keys():
+            if k == "time":
+                for v in output_values_dict[k]:
+                    t = root.find(f".//os:time[@type='{v}']", ns)
+                    sol_prob.append(t.text)
+            elif k == "other":
+                for v in output_values_dict[k]:
+                    val = root.find(f".//os:other[@name='{v}']", ns)
+                    sol_prob.append(val.attrib.get("value"))
+            
+        sol_list.append(sol_prob)
+    
+    # save values in csv
+    with open(os.path.join(output_dir,"soshyp_sol.csv"), "w", newline="", encoding="utf-8") as f:
+        writer = csv.writer(f)
+        writer.writerows(sol_list)
+
+
 
 
 if __name__ == "__main__":
